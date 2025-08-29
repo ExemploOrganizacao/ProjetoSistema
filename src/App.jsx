64f257e5987d0d95e38bc163ff938c9fab7af8c4
Felipe <febicaletto@gmail.com>
2025-08-29 09:52:37 -0300

    Criando nav do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {Error} from './routes/Error'
+import { Home } from './routes/Home'
+import { Cliente } from './routes/Cliente'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    //Habilita a navegação por rotas na aplicação
+    <BrowserRouter>
+      <main>
+        {/* Gerencia a exibição dos componentes com base na url */}
+        <Routes>
+          {/* Rota para lidar com as urls não encontradas */}
+          <Route path='*' element={<Error/>}/>
+
+          {/* Direciona para o Home */}
+          <Route path='/' element={<Home/>}/>
+
+          {/* Direciona para o Home */}
+          <Route path='/cliente' element={<Cliente/>}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Error = () => {
   return (
     <div>
-
+        <div>Errou404 Not Found</div>
     </div>
   )
 }
