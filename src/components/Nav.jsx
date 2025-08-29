f14e767880e28137183b6b499d1b0cd0efab0602
Felipe <febicaletto@gmail.com>
2025-08-29 09:54:02 -0300

    Criando nav do projeto

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import Logo from '../assets/mcqueen-serio.png'
 
-export const Nav = () => {
+const Nav = () => {
   return (
-    <div>
+    <header>
+        <nav>
+        <img src={Logo} alt="logo"/>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
 
-    </div>
+          <li>
+            <Link to="/cliente">Cliente</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
+
+export default Nav
--- src/routes/Cliente.jsx
+++ src/routes/Cliente.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Cliente = () => {
   return (
     <div>
-
+        Cliente
     </div>
   )
 }
