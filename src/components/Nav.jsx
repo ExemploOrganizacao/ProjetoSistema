0ac3135c5dace1d3d997a4455f52482b594fa60a
Felipe <febicaletto@gmail.com>
2025-09-01 09:05:24 -0300

    Estilizando o Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,16 +3,16 @@ import Logo from '../assets/mcqueen-serio.png'
 
 const Nav = () => {
   return (
-    <header>
-        <nav>
-        <img src={Logo} alt="logo"/>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+    <header className="bg-red-600 text-white p-4 shadow-md">
+        <nav className="container mx-auto flex justify-between items-center flex-row-reverse">
+        <img className="w-auto h-10" src={Logo} alt="logo"/>
+        <ul className="flex gap-2">
+          <li className="list-none">
+            <Link to="/" className="hover:text-yellow-400 transition-colors duration-300">Home</Link>
           </li>
 
-          <li>
-            <Link to="/cliente">Cliente</Link>
+          <li className="list-none">
+            <Link to="/cliente" className="hover:text-yellow-400 transition-colors duration-300">Cliente</Link>
           </li>
         </ul>
       </nav>
